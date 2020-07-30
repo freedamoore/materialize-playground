94833306c9f702dbf46b9e681182b56a20e61e96
Freeda Moore <freedamoore@outlook.com>
2020-07-30 15:52:58 +1000

    Updates

---
+++ precache-manifest.6c59894b45e02de6654b9bddd8de4520.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38aed2757b2cf7077e031c7673413493",
+    "url": "/materialize-playground/index.html"
+  },
+  {
+    "revision": "fb499c0c32395c4e4eb2",
+    "url": "/materialize-playground/static/css/main.bcc45858.chunk.css"
+  },
+  {
+    "revision": "558efaf8fb0b66d35bdc",
+    "url": "/materialize-playground/static/js/2.4cbcd2ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/materialize-playground/static/js/2.4cbcd2ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb499c0c32395c4e4eb2",
+    "url": "/materialize-playground/static/js/main.ea377260.chunk.js"
+  },
+  {
+    "revision": "f604fa8d2e0714dbd4a5",
+    "url": "/materialize-playground/static/js/runtime-main.3d05cf63.js"
+  }
+]);

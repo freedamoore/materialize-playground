cf321436259f6784ab309dd78891e8eb5ea5f496
Freeda Moore <freedamoore@outlook.com>
2020-07-30 16:15:29 +1000

    moved images from public folder into src/assets folder as webpack didn't push them to build

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import Card from './Card';
+import img1 from './../assets/boat.jpg';
+import img2 from './../assets/maldives.jpg';
+import img3 from './../assets/nature.jpg';
+import img4 from './../assets/road.jpg';
+import img5 from './../assets/yellowstone.jpg';
+import img6 from './../assets/bled.jpg';
+
 
 const CardList = () => {
     return (
         <div className="container">
             <div className="row">
-                <Card image="/images/boat.jpg" title="Lake District" />
-                <Card image="/images/maldives.jpg" title="Maldives" />
-                <Card image="/images/nature.jpg" title="Rivers and Mountains" />
-                <Card image="/images/road.jpg" title="Autumn Forest" />
-                <Card image="/images/yellowstone.jpg" title="Yellowstone Park" />
-                <Card image="/images/bled.jpg" title="Swampy Place" />
+                <Card image={img1} title="Lake District" />
+                <Card image={img2} title="Maldives" />
+                <Card image={img3} title="Rivers and Mountains" />
+                <Card image={img4} title="Autumn Forest" />
+                <Card image={img5} title="Yellowstone Park" />
+                <Card image={img6} title="Swampy Place" />
             </div>
 
         </div>
